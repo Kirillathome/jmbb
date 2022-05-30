d5a792df64e26e920a5956d00f7a59b4861c3e42
Kirillathome <kirip@outlook.de>
2022-05-30 20:42:36 +0200

    Update to 0.1.0

--- src/main/java/com/kirill/jmbb/JmbbMod.java
+++ src/main/java/com/kirill/jmbb/JmbbMod.java
@@ -1,15 +1,16 @@
-package com.example.example_mod;
+package com.kirill.jmbb;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class JmbbMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Just Mor Building Blocks");
+	public static final String Mod_ID = "jmbb";
 
 	@Override
 	public void onInitialize(ModContainer mod) {
